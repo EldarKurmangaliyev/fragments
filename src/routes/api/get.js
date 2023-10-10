ed812bb9fb3e66bfbb8ae046a9c010894fabce14
Eldar Kurmangaliyev <Ekurmangaliyev@myseneca.ca>
2023-10-09 21:40:02 -0500

    Assignment1

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,13 +1,23 @@
 // src/routes/api/get.js
-
-
-
-const { createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
-  res.status(200).json(createSuccessResponse({ fragments: [] }));
+const logger = require('../../logger');
+module.exports = async (req, res) => {
+  logger.debug('req.query: ' + JSON.stringify(req.query));
+  let expand;
+  try {
+    req.query.expand && req.query.expand === '1' ? (expand = true) : (expand = false);
+    const fragments = await Fragment.byUser(req.user, expand);
+    res.status(200).json(
+      createSuccessResponse({
+        fragments: fragments,
+      })
+    );
+  } catch (e) {
+    res.status(500).json(createErrorResponse(500, e));
+  }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ const { createSuccessResponse} = require('../response');
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all so you have to be authenticated in order to access.
